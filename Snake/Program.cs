aeb6546534ad5897ce5e72332822cc0b651d3cbe
gearszero <61977555+gearszero@users.noreply.github.com>
2024-03-11 17:19:11 +0100

    Used Enum, used stopwatch insted of date time, moved more stuff into functions

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -1,194 +1,240 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+
 ///█ ■
 ////https://www.youtube.com/watch?v=SGZgvMwjq2U
-namespace Snake
+namespace Snake;
+
+public enum Direction
 {
-    class Program
+    Up,
+    Down,
+    Left,
+    Right
+}
+
+class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Random randomNumber = new Random();
-            ScreenArea screenArea = new ScreenArea(32, 16);
-            //Pixel Positions
-            PixelsCoordination snakePixelsCoordination = new PixelsCoordination(screenArea.ScreenWidth/2, screenArea.ScreenHeight/2, ConsoleColor.Red);
-            PixelsCoordination berryPosition = new PixelsCoordination(randomNumber.Next(0, screenArea.ScreenWidth),
-                randomNumber.Next(0, screenArea.ScreenHeight), ConsoleColor.Yellow);
-            int score = 5;
-            bool gameOver = false;
-            string movement = "RIGHT";
-            List<int> xposlijf = new List<int>();
-            List<int> yposlijf = new List<int>();
+        Random randomNumber = new Random();
+        ScreenArea screenArea = new ScreenArea(32, 16);
 
-            DateTime tijd = DateTime.Now;
-            DateTime tijd2 = DateTime.Now;
-            string buttonpressed = "no";
-            while (true)
+        //Pixel Positions
+        PixelsCoordination snakeHead = new PixelsCoordination(screenArea.ScreenWidth / 2,
+            screenArea.ScreenHeight / 2, ConsoleColor.Red);
+        PixelsCoordination berryPosition = new PixelsCoordination(randomNumber.Next(0, screenArea.ScreenWidth),
+            randomNumber.Next(0, screenArea.ScreenHeight), ConsoleColor.Yellow);
+        List<PixelsCoordination> snakeBody = new List<PixelsCoordination>();
+
+        int score = 5;
+        Direction currentDirection = Direction.Right;
+
+
+        while (true)
+        {
+            Console.Clear();
+            GenerateScreenBorder(screenArea);
+            if (CheckIfBerryWasPickedUp(berryPosition, snakeHead))
             {
-                Console.Clear();
-                gameOver = CheckSnakeOutOfBounds(snakePixelsCoordination, screenArea);
-                GenerateScreenBorder(screenArea);
-                Console.ForegroundColor = ConsoleColor.Green;
-                score += CheckIfBerryWasPickedUp(snakePixelsCoordination, berryPosition, screenArea);
-                for (int i = 0; i < xposlijf.Count(); i++)
-                {
-                    Console.SetCursorPosition(xposlijf[i], yposlijf[i]);
-                    Console.Write("■");
-                    if (xposlijf[i] == snakePixelsCoordination.XPosition && yposlijf[i] == snakePixelsCoordination.YPosition)
-                    {
-                        gameOver = true;
-                    }
-                }
-                if (gameOver)
-                {
+                berryPosition = GenerateNewBerryPosition(berryPosition, screenArea);
+                ++score;
+            }
+
+            var gameOver = CheckSnakeOutOfBounds(snakeHead, screenArea) || CheckSnakeSelfCollision(snakeBody, snakeHead);
+            if (gameOver)
+            {
+                break;
+            }
+
+            DrawPixel(snakeHead);
+            DrawPixel(berryPosition);
+
+            var stopWatch = Stopwatch.StartNew();
+            while (stopWatch.ElapsedMilliseconds <= 500)
+            {
+                currentDirection = KeyInputChangeDirection(currentDirection);
+            }
+
+            snakeBody.Add(new PixelsCoordination(snakeHead.XPosition, snakeHead.YPosition, ConsoleColor.Green));
+
+            switch (currentDirection)
+            {
+                case Direction.Up:
+                    snakeHead.YPosition--;
                     break;
-                }
-                Console.SetCursorPosition(snakePixelsCoordination.XPosition, snakePixelsCoordination.YPosition);
-                Console.ForegroundColor = snakePixelsCoordination.GameConsoleColor;
-                Console.Write("■");
-                Console.SetCursorPosition(berryPosition.XPosition, berryPosition.YPosition);
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.Write("■");
-                tijd = DateTime.Now;
-                buttonpressed = "no";
-                while (true)
-                {
-                    tijd2 = DateTime.Now;
-                    if (tijd2.Subtract(tijd).TotalMilliseconds > 500) { break; }
-                    if (Console.KeyAvailable)
-                    {
-                        ConsoleKeyInfo toets = Console.ReadKey(true);
-                        //Console.WriteLine(toets.Key.ToString());
-                        if (toets.Key.Equals(ConsoleKey.UpArrow) && movement != "DOWN" && buttonpressed == "no")
-                        {
-                            movement = "UP";
-                            buttonpressed = "yes";
-                        }
-                        if (toets.Key.Equals(ConsoleKey.DownArrow) && movement != "UP" && buttonpressed == "no")
-                        {
-                            movement = "DOWN";
-                            buttonpressed = "yes";
-                        }
-                        if (toets.Key.Equals(ConsoleKey.LeftArrow) && movement != "RIGHT" && buttonpressed == "no")
-                        {
-                            movement = "LEFT";
-                            buttonpressed = "yes";
-                        }
-                        if (toets.Key.Equals(ConsoleKey.RightArrow) && movement != "LEFT" && buttonpressed == "no")
-                        {
-                            movement = "RIGHT";
-                            buttonpressed = "yes";
-                        }
-                    }
-                }
-                xposlijf.Add(snakePixelsCoordination.XPosition);
-                yposlijf.Add(snakePixelsCoordination.YPosition);
-                switch (movement)
-                {
-                    case "UP":
-                        snakePixelsCoordination.YPosition--;
-                        break;
-                    case "DOWN":
-                        snakePixelsCoordination.YPosition++;
-                        break;
-                    case "LEFT":
-                        snakePixelsCoordination.XPosition--;
-                        break;
-                    case "RIGHT":
-                        snakePixelsCoordination.XPosition++;
-                        break;
-                }
-                if (xposlijf.Count() > score)
-                {
-                    xposlijf.RemoveAt(0);
-                    yposlijf.RemoveAt(0);
-                }
-            }
-            Console.SetCursorPosition(screenArea.ScreenWidth / 5, screenArea.ScreenHeight / 2);
-            Console.WriteLine("Game over, Score: "+ score);
-            Console.SetCursorPosition(screenArea.ScreenWidth / 5, screenArea.ScreenHeight / 2 +1);
-        }
-
-        private static int CheckIfBerryWasPickedUp(PixelsCoordination berryPosition, PixelsCoordination snakePixelsCoordination,ScreenArea screenArea)
-        {
-            Random randomNumber = new Random();
-            if (berryPosition.XPosition == snakePixelsCoordination.XPosition && berryPosition.YPosition == snakePixelsCoordination.YPosition)
-            {
-                berryPosition.XPosition = randomNumber.Next(1, screenArea.ScreenWidth-2);
-                berryPosition.YPosition = randomNumber.Next(1, screenArea.ScreenHeight-2);
-                return 1;
+                case Direction.Down:
+                    snakeHead.YPosition++;
+                    break;
+                case Direction.Left:
+                    snakeHead.XPosition--;
+                    break;
+                case Direction.Right:
+                    snakeHead.XPosition++;
+                    break;
             }
 
-            return 0;
-        }
-
-        static void GenerateScreenBorder(ScreenArea screenArea)
-        {
-            for (int i = 0; i < screenArea.ScreenWidth; i++)
+            if (snakeBody.Count() > score)
             {
-                Console.SetCursorPosition (i, 0);
-                Console.Write("■");
-
-                Console.SetCursorPosition (i, screenArea.ScreenHeight - 1);
-                Console.Write ("■");
-            }
-
-            for (int i = 0; i < screenArea.ScreenHeight; i++)
-            {
-                Console.SetCursorPosition (0, i);
-                Console.Write ("■");
-
-                Console.SetCursorPosition (screenArea.ScreenWidth - 1, i);
-                Console.Write ("■");
+                snakeBody.RemoveAt(0);
             }
         }
 
-        static bool CheckSnakeOutOfBounds(PixelsCoordination currentSnakePosition, ScreenArea screenArea)
+        Console.SetCursorPosition(screenArea.ScreenWidth / 5, screenArea.ScreenHeight / 2);
+        Console.WriteLine("Game over, Score: " + score);
+        Console.SetCursorPosition(screenArea.ScreenWidth / 5, screenArea.ScreenHeight / 2 + 1);
+    }
+
+    private static bool CheckIfBerryWasPickedUp(PixelsCoordination berryPosition,
+        PixelsCoordination snakePixelsCoordination)
+    {
+        if (berryPosition.XPosition == snakePixelsCoordination.XPosition &&
+            berryPosition.YPosition == snakePixelsCoordination.YPosition)
         {
-            if (currentSnakePosition.XPosition == screenArea.ScreenWidth-1 || currentSnakePosition.XPosition == 0 ||currentSnakePosition.YPosition == screenArea.ScreenWidth-1 || currentSnakePosition.YPosition == 0)
-            { 
+            return true;
+        }
+
+        return false;
+    }
+
+    private static bool CheckSnakeOutOfBounds(PixelsCoordination currentSnakeHeadPosition, ScreenArea screenArea)
+    {
+        if (currentSnakeHeadPosition.XPosition == screenArea.ScreenWidth - 1 ||
+            currentSnakeHeadPosition.XPosition == 0 ||
+            currentSnakeHeadPosition.YPosition == screenArea.ScreenHeight - 1 || currentSnakeHeadPosition.YPosition == 0)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private static bool CheckSnakeSelfCollision(List<PixelsCoordination> snakeBody, PixelsCoordination snakeHead)
+    {
+        bool headCollide = false;
+        for (int bodyPixel = 0; bodyPixel < snakeBody.Count(); ++bodyPixel)
+        {
+            DrawPixel(snakeBody[bodyPixel]);
+            headCollide = (snakeBody[bodyPixel].XPosition == snakeHead.XPosition &&
+                         snakeBody[bodyPixel].YPosition == snakeHead.YPosition);
+            if (headCollide)
+            {
                 return true;
             }
+        }
+        return headCollide;
+    }
 
-            return false;
+    private static PixelsCoordination GenerateNewBerryPosition(PixelsCoordination berryPosition, ScreenArea screenArea)
+    {
+        Random randomNumber = new Random();
+        berryPosition = new PixelsCoordination(randomNumber.Next(1, screenArea.ScreenWidth - 2),
+            randomNumber.Next(1, screenArea.ScreenHeight - 2), ConsoleColor.Yellow);
+        DrawPixel(berryPosition);
+        return berryPosition;
+    }
+
+    private static void GenerateScreenBorder(ScreenArea screenArea)
+    {
+        PixelsCoordination borderCoordination = new PixelsCoordination(0, 0, ConsoleColor.Cyan);
+        for (int currentPosition = 0; currentPosition < screenArea.ScreenWidth; currentPosition++)
+        {
+            DrawPixel(borderCoordination.ChangePixelsPosition(currentPosition, 0));
+            DrawPixel(borderCoordination.ChangePixelsPosition(currentPosition, screenArea.ScreenHeight - 1));
         }
 
-        class PixelsCoordination
+        for (int currentPosition = 0; currentPosition < screenArea.ScreenHeight; currentPosition++)
         {
-            public PixelsCoordination(int xPosition, int yPosition, ConsoleColor color)
+            DrawPixel(borderCoordination.ChangePixelsPosition(0, currentPosition));
+            DrawPixel(borderCoordination.ChangePixelsPosition(screenArea.ScreenWidth - 1, currentPosition));
+        }
+    }
+
+    private static void DrawPixel(PixelsCoordination pixel)
+    {
+        Console.SetCursorPosition(pixel.XPosition, pixel.YPosition);
+        Console.ForegroundColor = pixel.PixelColor;
+        Console.Write("■");
+        Console.SetCursorPosition(0, 0);
+    }
+
+    private static Direction KeyInputChangeDirection(Direction currentDirection)
+    {
+        if (Console.KeyAvailable)
+        {
+            var readKey = Console.ReadKey(true).Key;
+            if (readKey == ConsoleKey.UpArrow && currentDirection != Direction.Down)
             {
-                XPosition = xPosition;
-                YPosition = yPosition;
-                GameConsoleColor = color;
+                currentDirection = Direction.Up;
             }
 
-            public int XPosition { get; set; }
-            public int YPosition { get; set; }
-            public ConsoleColor GameConsoleColor { get; set; }
+            else if (readKey == ConsoleKey.DownArrow && currentDirection != Direction.Up)
+            {
+                currentDirection = Direction.Down;
+            }
+
+            else if (readKey == ConsoleKey.LeftArrow && currentDirection != Direction.Right)
+            {
+                currentDirection = Direction.Left;
+            }
+
+            else if (readKey == ConsoleKey.RightArrow && currentDirection != Direction.Left)
+            {
+                currentDirection = Direction.Right;
+            }
         }
 
-        class ScreenArea
-        {
-            public ScreenArea(int screenWidth, int screenHeight)
-            {
-                ScreenWidth = screenWidth;
-                ScreenHeight = screenHeight;
-                SetScreenWidth(ScreenWidth,ScreenHeight);
-            }
+        return currentDirection;
+    }
 
-            public int ScreenWidth { get; private set; }
-            public int ScreenHeight { get; private set; }
-            
-            //functions
-            private static void SetScreenWidth(int screenWidth, int screenHeight)
-            {
-                Console.WindowWidth = screenWidth;
-                Console.WindowHeight = screenHeight;
-            }
+    class PixelsCoordination
+    {
+        public PixelsCoordination(int xPosition, int yPosition, ConsoleColor color)
+        {
+            XPosition = xPosition;
+            YPosition = yPosition;
+            PixelColor = color;
+        }
+
+        public int XPosition { get; set; }
+        public int YPosition { get; set; }
+        public ConsoleColor PixelColor { get; set; }
+
+        //functions
+        public PixelsCoordination ChangePixelsPosition(int newPositionX, int newPositionY)
+        {
+            return new PixelsCoordination(newPositionX,newPositionY, this.PixelColor);
+        }
+        
+        public void ChangePixelLocation(int newLocationX, int newLocationY)
+        {
+            XPosition = newLocationX;
+            YPosition = newLocationY;
+        }
+    }
+
+    class ScreenArea
+    {
+        public ScreenArea(int screenWidth, int screenHeight)
+        {
+            ScreenWidth = screenWidth;
+            ScreenHeight = screenHeight;
+            SetScreenWidth(ScreenWidth, ScreenHeight);
+        }
+
+        public int ScreenWidth { get; private set; }
+        public int ScreenHeight { get; private set; }
+
+        //functions
+        private static void SetScreenWidth(int screenWidth, int screenHeight)
+        {
+            Console.WindowWidth = screenWidth;
+            Console.WindowHeight = screenHeight;
         }
     }
 }
